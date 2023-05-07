35da70939a4f51bebcb762725142788a9a01d778
tekelala <camilo@tekelala.com>
2023-05-07 11:03:54 -0500

    v1

---
+++ claude.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+def send_message(prompt):
+    api_url = "https://api.anthropic.com/v1/complete"
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
+    }
+
+    # Prepare the prompt for Claude
+    conversation = f"Human: {prompt}\n\nAssistant:"
+
+    # Define the body of the request
+    body = {
+        "prompt": conversation,
+        "model": "claude-v1.3",
+        "max_tokens_to_sample": 500,
+        "stop_sequences": ["\n\nHuman:"]
+    }
+
+    # Make a POST request to the Claude API
+    try:
+        response = requests.post(api_url, headers=headers, data=json.dumps(body))
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        st.error(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        st.error(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        st.error(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        st.error(f"Something went wrong: {err}")
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
+
+    # Extract Claude's response from the JSON response
+    result = response.json()
+
+    # Return Claude's response as a string
+    return result['completion']
