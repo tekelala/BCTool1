949e66ae4b1e46e32cc022b37640fbd66858ca95
tekelala <camilo@tekelala.com>
2023-05-07 14:23:01 -0500

    should work

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import claude as cl
 if "email" not in st.session_state:
     st.session_state.email = ""
     st.session_state.prompt = ""
-    st.session_state.user_name_input = ""
-    st.session_state.destinatary = ""
-    st.session_state.user_multiselect_speech_act = ""
-    st.session_state.message_user = ""
-    st.session_state.message_tone = ""
 
 # Container 1: Image and Title
 with st.container():
@@ -20,7 +15,7 @@ with st.container():
 # Container 2: User Input
 with st.container():
     if st.session_state.email == "":
-        with st.form(key='email_form'):
+        with st.form(key='user_input_form'):
             user_name_input = st.text_input("What is your name:", key="user_name_input")
             destinatary = st.text_input("Who will receive this email:", key="destinatary")
             user_speech_act = st.selectbox("The main purpose of the message is to make:", ['an assertion','a promise', 'a request'], key="user_multiselect_speech_act")
@@ -30,12 +25,12 @@ with st.container():
 
             if submit_button_container_2 and user_name_input and destinatary and user_speech_act and message_user and message_tone:
                 st.session_state.user_name = user_name_input
-                st.session_state.prompt = f"You are a communications expert using the speech acts of Fernando FLores and you are going to write an email to {destinatary} with the following speech act: {user_speech_act}, with the following {message_tone} the email should transmit the following message {message_user}.\n\n The person that sends the email is: {st.session_state.user_name} Never mention that you use speech acts or that you use a tool to write the email."
+                st.session_state.prompt = f"role: you are a communications expert using the speech acts of Fernando Flores \n  you are going to write an email to {destinatary} with the following speech act: {user_speech_act}, with the following {message_tone} to deliver the following message {message_user}.\n\n Best regards,\n\n {st.session_state.user_name}"
                 with st.spinner('Generating email...'):
                     st.session_state.email = cl.send_message(st.session_state.prompt)
 
 
-# Container 3: Response
+## Container 3: Response
 with st.container():
     if st.session_state.email != "":
         st.write(f"Here is the email that was generated:")
@@ -49,12 +44,14 @@ with st.container():
             submit_button = st.form_submit_button(label='Send')
             reset_button = st.form_submit_button(label='Restart')
 
-            if submit_button and user_text.strip():
-                st.session_state.prompt += f" Please change the email as follows: {user_text.strip()} \n\n remember that the email is sent by \n {st.session_state.user_name}. Write only the email content. Never mention that you use speech acts or that you use a tool to write the email."
+            if submit_button and user_text:
+                st.session_state.prompt += f"Please change the email as follows: {user_text} \n\n remember that the email is sent by {st.session_state.user_name}. Never mention that you use speech acts."
                 with st.spinner('Modifying email...'):
                     st.session_state.email = cl.send_message(st.session_state.prompt)
+                st.experimental_rerun()
 
             if reset_button:
                 st.session_state.prompt = ""
                 st.session_state.email = ""
                 st.session_state.user_name = ""
+                st.experimental_rerun()
