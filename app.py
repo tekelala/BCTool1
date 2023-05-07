f3890f2e186dae7ef3a0f32bc8f3070b6b2e245f
tekelala <camilo@tekelala.com>
2023-05-07 11:48:23 -0500

    working

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ with st.container():
             submit_button_container_2 = st.form_submit_button(label='Send')
 
             if submit_button_container_2 and user_name and destinatary and user_speech_act and message_user and message_tone:
-                st.session_state.prompt = f"role: you are a communications expert using the speech acts of Fernando Flores \n  you are going to write an email to {destinatary} with the following speech act: {user_speech_act}, with the following {message_tone} to deliver the following message {message_user}.\n\nBest regards,\n\n {user_name}"
+                st.session_state.prompt = f"role: you are a communications expert using the speech acts of Fernando Flores \n  you are going to write an email to {destinatary} with the following speech act: {user_speech_act}, with the following {message_tone} to deliver the following message {message_user}.\n\n Best regards,\n\n {user_name}"
                 st.session_state.email = cl.send_message(st.session_state.prompt)
 
 # Container 3: Response
@@ -42,7 +42,7 @@ with st.container():
             reset_button = st.form_submit_button(label='Restart')
 
             if submit_button and user_text:
-                st.session_state.prompt += f"Please change the email as follows: {user_text}"
+                st.session_state.prompt += f"Please change the email as follows: {user_text} \n\n remember that the email is sent by {user_name}. Never mention that you use speech acts."
                 st.session_state.email = cl.send_message(st.session_state.prompt)
                 st.experimental_rerun()
 
